fb1fdc41498d04487831a348f2faa454fea4cc25
Alexander <plaxon@Live.ru>
2017-11-21 03:16:41 +0300

    wip

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -3,10 +3,30 @@ import './modules';
 
 global.jQuery = global.$ = require('jquery');
 var slick = require('slick-carousel');
+var inputmask = require('inputmask/dist/min/jquery.inputmask.bundle.min');
 var fancy = require('fancy');
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-$('.med-image, .studio-image').fancybox({
+setInterval(
+    function mainSlider() {
+        var random = getRandomInt(0, 16);
+
+        var $li = $(".mainSlider .photoSlider");
+        var $liActive = $(".mainSlider .bg-on");
+
+        $li.eq(random % $li.length).addClass("bg-on");
+        $liActive.eq(random/2 % $liActive.length).removeClass("bg-on");
+
+    }, 3000);
+
+$(".input").inputmask({
+    mask: ["+7 (999) 999-99-99"]
+});
+
+$('.med-image, .studio-image, .photoSlider').fancybox({
     buttons : [
         //'slideShow',
         //'fullScreen',
@@ -16,6 +36,23 @@ $('.med-image, .studio-image').fancybox({
         'close'
     ],
     loop: true
+});
+
+$('.service-el_button').on('click', function () {
+    $(".formInput").inputmask({
+        mask: ["+7 (999) 999-99-99"]
+    });
+    $.fancybox.open({
+            type: 'inline',
+            src: '#service-request',
+        touch : false
+    })
+});
+
+$('.collective-carousel, .instagram-carousel, .youtube-carousel').slick({
+    lazyLoad: 'ondemand',
+    dots: true,
+    infinite: true
 });
 
 var url = 'https://api.vk.com/method/board.getComments?group_id=54002395&topic_id=29356060&v=5.69';
