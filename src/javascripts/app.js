091dd048fbb3ce012e99a4482e49631fba45c505
Alexander <plaxon@Live.ru>
2017-11-22 01:13:53 +0300

    wip

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -5,6 +5,8 @@ global.jQuery = global.$ = require('jquery');
 var slick = require('slick-carousel');
 var inputmask = require('inputmask/dist/min/jquery.inputmask.bundle.min');
 var fancy = require('fancy');
+var jm = require('jquery-mousewheel');
+var scrollbar = require('malihu-custom-scrollbar-plugin');
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -18,7 +20,7 @@ setInterval(
         var $liActive = $(".mainSlider .bg-on");
 
         $li.eq(random % $li.length).addClass("bg-on");
-        $liActive.eq(random/2 % $liActive.length).removeClass("bg-on");
+        $liActive.eq(random / 2 % $liActive.length).removeClass("bg-on");
 
     }, 3000);
 
@@ -27,7 +29,7 @@ $(".input").inputmask({
 });
 
 $('.med-image, .studio-image, .photoSlider').fancybox({
-    buttons : [
+    buttons: [
         //'slideShow',
         //'fullScreen',
         //'thumbs',
@@ -43,23 +45,144 @@ $('.service-el_button').on('click', function () {
         mask: ["+7 (999) 999-99-99"]
     });
     $.fancybox.open({
-            type: 'inline',
-            src: '#service-request',
-        touch : false
+        type: 'inline',
+        src: '#service-request',
+        touch: false
     })
 });
 
-$('.collective-carousel, .instagram-carousel, .youtube-carousel').slick({
+$('.instagram-carousel').slick({
     lazyLoad: 'ondemand',
     dots: true,
-    infinite: true
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+});
+$('.collective-carousel, .youtube-carousel').slick({
+    lazyLoad: 'ondemand',
+    dots: true,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }
+    ]
 });
 
-var url = 'https://api.vk.com/method/board.getComments?group_id=54002395&topic_id=29356060&v=5.69';
-$.ajax({
-    url: url,
-    dataType: "jsonp",
-    success : function(msg){
-        console.log(msg.response[0]);
-    }
-});
+(function vkFeedback() {
+    var vkCarousel = $('.vk-carousel');
+    $.ajax({
+        url: 'https://api.vk.com/method/board.getComments?group_id=54002395&topic_id=29356060&extended=1&sort=desc&count=35&v=5.69',
+        type: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+            var items = data.response.items,
+                profiles = data.response.profiles,
+                textNice = $('.vkItem-text-posa');
+
+            for (var i = 0; i < items.length; i++) {
+                for (var j = 0; j < profiles.length; j++) {
+
+                    if(profiles[j].id === 329031346) {
+                        delete profiles[j].id
+                    }
+                    if(profiles[j].id === 306924120) {
+                        delete profiles[j].id
+                    }
+                    if(profiles[j].id === 133410127) {
+                        delete profiles[j].id
+                    }
+                    if(profiles[j].id === 74440225) {
+                        delete profiles[j].id
+                    }
+                    if(profiles[j].id === 154139772) {
+                        delete profiles[j].id
+                    }
+                    if(items[i].text === '') {
+                        if(profiles[j].id === 6262936) {
+                            delete profiles[j].id
+                        }
+                    }
+                    console.log(profiles[j].id);
+                    if (profiles[j].id === items[i].from_id) {
+                        items[i].first_name = profiles[j].first_name;
+                        items[i].last_name = profiles[j].last_name;
+                        items[i].photo_100 = profiles[j].photo_100;
+                        items[i].linkVK = 'id' + items[i].from_id;
+
+                        vkCarousel.append(
+                            '<div class="vkItem">'
+                                +'<div class="vkItem-header">'
+                                    + '<div class="vkItem-left" style="background-image: url('+ items[i].photo_100 +')">'+'</div>'
+                                    + '<div class="vkItem-right">'
+                                        + '<div class="vkItem-fname">'+ items[i].first_name +'</div>'
+                                        + '<div class="vkItem-lname">'+ items[i].last_name +'</div>'
+                                        + '<a class="vkItem-link" href="https://vk.com/' + items[i].linkVK + '" target="_blank">'+ items[i].linkVK +'</a>'
+                                    +'</div>'
+                                +'</div>'
+                                +'<div class="vkItem-text">'
+                                    +'<div class="vkItem-text-posa">'
+                                        +'<span class="vkItem-text-posa-span">'+ items[i].text.replace(/ *\[[^)]*\], */g, "") +'</span>'
+                                    +'</div>'
+                                +'</div>'+
+                            '</div>'
+                        );
+                    }
+                }
+            }
+        }
+    }).done(function () {
+        vkCarousel.slick({
+            dots: true,
+            infinite: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        arrows: false
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false
+                    }
+                }
+            ]
+        }).init();
+        $('.vkItem-text-posa').mCustomScrollbar({
+            autoDraggerLength: false,
+            live: 'on',
+            mouseWheel:{ enable: true, preventDefault: true }
+        });
+    })
+})();
